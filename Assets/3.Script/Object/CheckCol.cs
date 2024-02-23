3bdc0393bfa967b8d06c6b1513e6a18d685e3633
Limkwan1004 <janyouns@gmail.com>
2024-02-23 17:59:33 +0900

    [Feat] CheckCol

--- Assets/3.Script/Object/CheckCol.cs
+++ Assets/3.Script/Object/CheckCol.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class CheckCol : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool isCheck = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if(collision != null)
+        {
+            isCheck = true;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
